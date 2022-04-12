06043b001b02473d979a337e953d354c37913b1c
Juan Manuel González <juan.m-gonzalez@outlook.com>
2022-04-12 18:03:20 -0300

    pruebas

--- models/Movement.js
+++ models/Movement.js
@@ -2,8 +2,8 @@ const { model, Schema } = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const movementSchema = new Schema({
-  amount: { type: Number },
-  description: { type: String, required: false },
+  amount: Number,
+  description: String,
   date: { type: Date, default: Date.now },
   user: {
     type: Schema.Types.ObjectId,
@@ -16,8 +16,6 @@ movementSchema.set('toJSON', {
     returnedObject.id = returnedObject._id
     delete returnedObject._id
     delete returnedObject.__v
-
-    delete returnedObject.passwordHash
   }
 })
 
--- models/User.js
+++ models/User.js
@@ -2,10 +2,10 @@ const { model, Schema } = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new Schema({
-  name: { type: String, required: false },
-  lastName: { type: String, required: false },
-  email: { type: String, required: false },
-  passwordHash: { type: String, required: false },
+  name: String,
+  lastName: String,
+  email: String,
+  passwordHash: String,
   date: { type: Date, default: Date.now },
   balance: { type: Number, default: 50000 },
   movements: [{
