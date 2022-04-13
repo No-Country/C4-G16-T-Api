3c809fd5af5f874f425cb26367781cf758cfa69a
Juan Manuel González <juan.m-gonzalez@outlook.com>
2022-04-12 23:15:06 -0300

    ya funcionando

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const { model, Schema } = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new Schema({
   name: String,
@@ -23,8 +22,6 @@ userSchema.set('toJSON', {
     delete returnedObject.passwordHash
   }
 })
-
-userSchema.plugin(uniqueValidator)
 
 const User = model('User', userSchema)
 
