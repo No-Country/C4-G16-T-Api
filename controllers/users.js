52c713d9180616c5b854eaa74e05a0542c777f72
JUAN MANUEL GONZALEZ <jm.gonzalez@valkimia.com>
2024-02-12 20:18:10 -0300

    users correction

--- controllers/users.js
+++ controllers/users.js
@@ -2,33 +2,24 @@ const usersRouter = require('express').Router()
 const userExtractor = require('../middlewares/userExtractor')
 const User = require('../models/User')
 
-usersRouter.get('/', async (req, res) => {
-  const users = await User.find({}).populate('movements', {
-    amount: 1,
-    description: 1,
-    date: 1
-  })
-  res.json(users)
-})
-
-usersRouter.get('/:id', async (req, res, next) => {
-  const { id } = req.params
+usersRouter.get("/:id", userExtractor, async (req, res, next) => {
+  const { id } = req.params;
 
   try {
-    const user = await User.findById(id).populate('movements', {
+    const user = await User.findById(id).populate("movements", {
       amount: 1,
       description: 1,
-      date: 1
-    })
+      date: 1,
+    });
     if (user) {
-      res.status(200).send(user)
+      res.status(200).send(user);
     } else {
-      res.status(404).end()
+      res.status(404).end();
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 usersRouter.put('/:id', userExtractor, async (req, res, next) => {
   const { id } = req.params
